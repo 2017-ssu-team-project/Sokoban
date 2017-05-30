72cdca7812503a524cacfe9c60aaf630e6549af4
yeongchan <cyc03265@naver.com>
2017-05-30 19:29:45 +0900

    Delete manual.c

--- manual.c
+++
@@ -1,6 +0,0 @@
-int manual()  {  
-  int i = 0;   char manual[150] = {0,};//파일을 읽을 때 사용할 임시 공간  
-               FILE *mu = fopen("manual.txt", "r"); 
-               while (fgets(manual, sizeof(manual), mu) != NULL)  
-               {     printf("%s", manual);   }  
-               fclose(mu);   return 0;  }
